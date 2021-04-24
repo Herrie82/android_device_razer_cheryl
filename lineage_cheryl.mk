eadcfb63fbc4081f9a544d82670210ec04b570dd
Herrie <Github.com@herrie.org>
2021-04-24 22:51:39 +0200

    lineage_cheryl.mk: Build only Halium bits

--- lineage_cheryl.mk
+++ lineage_cheryl.mk
@@ -22,10 +22,11 @@ TARGET_SCREEN_WIDTH := 1440
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/razer/cheryl/device.mk)
